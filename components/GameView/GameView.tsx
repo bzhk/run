4b69553dd334d298da1f51ebd941df63e8fd3be2
Bartosz <bzhk911@gmail.com>
2019-04-14 00:20:40 +0200

    new board render

--- components/GameView/GameView.tsx
+++ components/GameView/GameView.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
 import config from "../data/board";
 import Player from "./Player";
+import Board from "./Board";
 interface Props {}
 interface State {
   level: number;
@@ -9,98 +10,70 @@ interface State {
   playerIndexR: number;
   playerIndexC: number;
   checkCords: boolean;
+  screenWidth: number;
+  boardWidth: number;
+  boardMargin: number;
+  cellWidth: number;
 }
 
 export default class GameView extends Component<Props, State> {
-  private _startCords: any[][];
-
   constructor(props: Props) {
     super(props);
+    const width: number = Math.round(Dimensions.get("window").width);
+    const boardWidth: number = width * 0.8;
+    const boardMargin: number = width * 0.1;
+    const cellWidth: number = boardWidth / 9;
     this.state = {
       checkCords: false,
       level: 1,
       board: [],
       playerIndexR: 0,
-      playerIndexC: 0
+      playerIndexC: 0,
+      screenWidth: width,
+      boardWidth: boardWidth,
+      cellWidth: cellWidth,
+      boardMargin: boardMargin
     };
-
-    this._startCords = [];
   }
-
-  setCordsArray = (
-    ref: any,
-    cell: string,
-    r_index: number,
-    c_index: number
-  ): void => {
-    setTimeout(() => {
-      if (cell === "S") {
-        this._startCords = { ref: ref, x: r_index, y: c_index };
-      }
-    }, 100);
-  };
-
-  getCords = (): void | any[][] => {
-    if (this._startCords) return this._startCords;
-  };
-
-  checkCords = (): void => {
-    setTimeout(() => {
-      const cords = this.getCords();
-      if (cords) {
-        this.setState({
-          checkCords: true
-        });
-      } else {
-        this.checkCords();
-      }
-    }, 500);
-  };
 
   componentDidMount(): void {
     const levels: { [key: number]: string[][] } = config.levels;
     const index: number = this.state.level;
     let board = levels[index];
+    this.setStartCords(board);
     this.setState({
       board: board
     });
-    this.checkCords();
   }
 
-  render() {
-    console.log(this.getCords());
+  setStartCords = (board: string[][]): void => {
+    board.forEach((row: string[], r_index: number) => {
+      row.forEach((cell: string, c_index: number) => {
+        if (cell == "S") {
+          this.setState({
+            playerIndexR: r_index,
+            playerIndexC: c_index
+          });
+          return;
+        }
+      });
+    });
+  };
 
+  render() {
     return (
       <View style={styles.container}>
-        {!this.state.checkCords && (
-          <View style={styles.loader}>
-            <Text>Loading...</Text>
-          </View>
-        )}
-        {this.state.checkCords && (
-          <Player
-            getCords={this.getCords}
-            rIndex={this.state.playerIndexR}
-            cIndex={this.state.playerIndexC}
-          />
-        )}
-        {this.state.board.map((row, r_index) => {
-          return (
-            <View style={styles.row} key={r_index}>
-              {row.map((cell, c_index) => {
-                return (
-                  <View
-                    style={styles.cell}
-                    key={c_index}
-                    ref={ref => this.setCordsArray(ref, cell, r_index, c_index)}
-                  >
-                    <Text>{cell}</Text>
-                  </View>
-                );
-              })}
-            </View>
-          );
-        })}
+        <Player
+          rIndex={this.state.playerIndexR}
+          cIndex={this.state.playerIndexC}
+          cellWidth={this.state.cellWidth}
+          boardMargin={this.state.boardMargin}
+        />
+        <Board
+          boardMargin={this.state.boardMargin}
+          cellWidth={this.state.cellWidth}
+          board={this.state.board}
+        />
       </View>
     );
   }
@@ -110,33 +83,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: "100%",
-    justifyContent: "center",
-
     backgroundColor: "#F5FCFF"
-  },
-  loader: {
-    height: "100%",
-    width: "100%",
-    backgroundColor: "#fff",
-    zIndex: 100
-  },
-  header: {
-    fontSize: 30,
-    textAlign: "center",
-    margin: 10
-  },
-  row: {
-    width: "100%",
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center"
-  },
-  cell: {
-    flex: 1,
-    borderRadius: 2,
-    borderWidth: 1,
-    borderColor: "#d6d7da",
-    justifyContent: "center",
-    alignItems: "center"
   }
 });
--- components/GameView/Player.tsx
+++ components/GameView/Player.tsx
@@ -2,70 +2,29 @@ import React, { Component } from "react";
 import { Image, View, Button } from "react-native";
 const hero: any = require("../assets/hero.png");
 interface Props {
-  getCords: () => any;
   rIndex: number;
   cIndex: number;
+  cellWidth: number;
+  boardMargin: number;
 }
-interface State {
-  pX: number;
-  pY: number;
-  pWidth: number;
-  pHeight: number;
-  hAvatar: number;
-}
+interface State {}
 export default class Player extends Component<Props, State> {
-  private getCords: () => any;
   constructor(props: Props) {
     super(props);
-    this.state = {
-      pX: 0,
-      pY: 0,
-      pWidth: 0,
-      pHeight: 0,
-      hAvatar: 40
-    };
-    console.log(props);
-    this.getCords = this.props.getCords;
-  }
-
-  componentDidMount() {
-    const _player = this.getCords();
-    _player.ref.measure(
-      (
-        fx: number,
-        fy: number,
-        width: number,
-        height: number,
-        px: number,
-        py: number
-      ): void => {
-        console.log(py - height + (height - this.state.hAvatar) / 2);
-        console.log(
-          `${py} - ${height} + (${height} - ${this.state.hAvatar}) / 2`
-        );
-
-        this.setState({
-          pX: px,
-          pY: py - this.state.hAvatar * 2,
-          pWidth: width,
-          pHeight: height
-        });
-      }
-    );
   }
 
   render() {
-    const { pX, pY, pWidth, hAvatar } = this.state;
-    console.log(pX, pY, pWidth, hAvatar);
+    const { rIndex, cIndex, cellWidth, boardMargin } = this.props;
+
     return (
       <Image
         source={hero}
         style={{
-          width: pWidth,
-          height: hAvatar,
+          width: cellWidth,
+          height: cellWidth,
           position: "absolute",
-          left: pX,
-          top: pY,
+          left: cellWidth * cIndex + boardMargin,
+          top: cellWidth * rIndex + boardMargin * 2,
           zIndex: 10,
           borderRadius: 2,
           borderWidth: 1,
